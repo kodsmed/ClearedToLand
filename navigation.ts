9bfb24616ec2e87b17636b89bcc3f1ee80f9ddf5
Jimmy Karlsson <jk224jv@student.lnu.se>
2025-04-27 13:04:16 +0200

    add navigational classes

---
+++ navigation.ts
@@ -0,0 +1,55 @@
+export interface Point {
+  getCoordinates(): {x: number, y: number, z: number}
+}
+
+export class Position implements Point{
+  // Properties
+  private x: number;
+  private y: number;
+  private z: number;
+  constructor (args: {x: number, y: number, z: number}) {
+    this.x = args.x;
+    this.y = args.y;
+    this.z = args.z;
+  }
+
+  getCoordinates() {
+    return {x: this.x, y: this.y, z: this.z}
+  }
+}
+
+export class Approach implements Point {
+  private position: Position;
+  private navTarget: NavPoint;
+
+  Approach(args: {position: Position, navTarget: NavPoint}) {
+    this.position = args.position
+    this.navTarget = args.navTarget
+  }
+
+  getCoordinates() {
+    return this.position.getCoordinates()
+  }
+
+  getNavTarget() {
+    return this.navTarget
+  }
+}
+
+export class NavPoint implements Point {
+  private name: string;
+  private position: Position;
+
+  NavPoint(args: {name: string, position:Position}) {
+    this.name = args.name
+    this.position = args.position
+  }
+
+  getName() : string {
+    return `${this.name}`
+  }
+
+  getCoordinates(): { x: number; y: number; z: number; } {
+      return this.position.getCoordinates()
+  }
+}
