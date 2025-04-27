b79e4600bced5321f95bf49b1d27febdd0ee5cdb
Jimmy Karlsson <jk224jv@student.lnu.se>
2025-04-27 13:22:40 +0200

    add pilot behaviour when entering mapp

--- pilot.ts
+++ pilot.ts
@@ -1,24 +1,33 @@
-enum ACTION {
-  PROCEED,
-  HOLD_AT,
-  DESCEND,
-  ASCEND,
-  TAKEOFF,
-  LAND,
-}
+import { Plane } from './plane.ts'
+import { NavPoint, Approach, Position } from './navigation.ts'
+import { ACTION, MOVEMENT_TYPE } from './enums.ts'
+import { AnimationFunctions } from './ui.ts'
 
-class Pilot {
+
+export class Pilot {
   private plane : Plane;
   private action: ACTION;
   private navTarget: NavPoint;
+  private Iui: AnimationFunctions;
 
-  Pilot(myAircraft: Plane) {
+  Pilot(myAircraft: Plane, Iui: AnimationFunctions) {
     this.plane = myAircraft;
+    this.Iui
 
   }
 
   enterFromApproach(approach: Approach){
-    //Get heading by comparing the approach position and the navpoint position
-    //update movement
+
+    this.plane.movement.setMovementType(MOVEMENT_TYPE.CRUISING)
+    this.plane.movement.setHeading(this.getHeadingByApproach(approach))
+    this.Iui.startPlaneMovementAnimation(this.plane)
+  }
+
+  private getHeadingByApproach(approach:Approach) : number {
+    const aCoords : {x: number, y: number, z: number} = approach.getCoordinates()
+    const bCoords : {x: number, y: number, z: number} = approach.getNavTargetCoordinates()
+    const xDifference: number = aCoords.x - bCoords.x
+    const yDifference: number = aCoords.y - bCoords.y
+    return Math.atan2(yDifference, xDifference)
   }
 }
