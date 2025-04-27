b58c05dcdd90e2ec8bb0563c400e7c7cd307f25b
Jimmy Karlsson <jk224jv@student.lnu.se>
2025-04-27 12:43:18 +0200

    RRMartin refactor

--- airspace.ts
+++ airspace.ts
@@ -1,32 +1,8 @@
-class Approach {
-  private position: Position;
-  private navTarget: NavPoint;
+import { Plane } from "./plane.ts";
+import { Approach } from "./navigation.ts";
+import { PLANE_TYPE } from "./enums.ts";
 
-  Approach(args: {position: Position, navTarget: NavPoint}) {
-    this.position = args.position
-    this.navTarget = args.navTarget
-  }
-
-  getPosition() {
-    return this.position
-  }
-
-  getNavTarget() {
-    return this.navTarget
-  }
-}
-
-class NavPoint {
-  name: string;
-  position: Position;
-
-  NavPoint(args: {name: string, position:Position}) {
-    this.name = args.name
-    this.position = args.position
-  }
-}
-
-class Airspace {
+export class Airspace {
   private planes: Plane[];
   private approaches: Approach[];
   //private runways: Runway[];
@@ -42,17 +18,8 @@ class Airspace {
 
   }
 
-
   addPlaneToApproach(args: {fuelEmergency?: boolean}): void {
-
-    // randomly select a plane type
-    const planeTypeKeys = Object.keys(PLANE_TYPE);
-    const randomPlaneTypeKey = planeTypeKeys[Math.floor(Math.random() * planeTypeKeys.length)];
-    const randomPlaneType = PLANE_TYPE[randomPlaneTypeKey as keyof typeof PLANE_TYPE];
-    const randomApproachKey = Math.floor(Math.random() * this.approaches.length)
-    const randomApproach = this.approaches[randomApproachKey]
-
-    this.planes.push(new Plane({id: this.numberOfPlanesTotal, planeModel: randomPlaneType, approach: randomApproach, fuelEmergency: args.fuelEmergency || false}));
+    this.planes.push(new Plane({id: this.numberOfPlanesTotal, planeModel: this.getRandomPlaneType(), approach: this.getRandomApproach(), fuelEmergency: args.fuelEmergency || false}));
     this.incrementNumberOfPlanesTotal();
   }
 
@@ -69,5 +36,18 @@ class Airspace {
 
   getNumberOfPlanesTotal(): number {
     return this.numberOfPlanesTotal;
+  }
+
+  private getRandomPlaneType(): PLANE_TYPE {
+    const planeTypeKeys = Object.keys(PLANE_TYPE);
+    const randomPlaneTypeKey = planeTypeKeys[Math.floor(Math.random() * planeTypeKeys.length)];
+    const randomPlaneType = PLANE_TYPE[randomPlaneTypeKey as keyof typeof PLANE_TYPE];
+    return randomPlaneType
+  }
+
+  private getRandomApproach(): Approach {
+    const randomApproachKey = Math.floor(Math.random() * this.approaches.length)
+    const randomApproach = this.approaches[randomApproachKey]
+    return randomApproach
   }
 }
